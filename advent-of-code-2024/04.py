dc91dfc3ade4a4ad0afe98684dd0260bcb68e3d0
Krzysztof <krzysztof.turek007@gmail.com>
2024-12-15 12:46:26 +0100

    04/2024

--- advent-of-code-2024/04.py
+++ advent-of-code-2024/04.py
@@ -38,6 +38,38 @@ def counter(grid):
     return count
 
 
+def counter_x(grid):
+    x = len(grid)  # n
+    y = len(grid[0])  # m
+    count = 0
+
+    def is_valid(r, c):
+        return 0 <= r < x and 0 <= c < y
+
+    for r in range(1, x - 1):
+        # r - row number
+        for c in range(1, y - 1):
+            # c - column number
+            if (is_valid(r - 1, c - 1) and is_valid(r - 1, c + 1) and
+                    is_valid(r + 1, c - 1) and is_valid(r + 1, c + 1)):
+
+                top_left = grid[r - 1][c - 1]
+                top_right = grid[r - 1][c + 1]
+                center = grid[r][c]
+                bottom_left = grid[r + 1][c - 1]
+                bottom_right = grid[r + 1][c + 1]
+
+                if center == 'A':
+                    if ((top_left == 'M' and top_right == 'M' and bottom_left == 'S' and bottom_right == 'S')
+                            or (top_left == 'S' and top_right == 'S' and bottom_left == 'M' and bottom_right == 'M')
+                            or (top_left == 'M' and top_right == 'S' and bottom_left == 'M' and bottom_right == 'S')
+                            or (top_left == 'S' and top_right == 'M' and bottom_left == 'S' and bottom_right == 'M')
+                    ):
+                        count += 1
+
+    return count
+
+
 grid = []
 
 while True:
@@ -50,3 +82,4 @@ while True:
         break
 
 print(counter(grid))
+print(counter_x(grid))
