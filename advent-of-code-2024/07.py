2c0a8fce77161559c3b9a0bfa20a3d44861b193b
Krzysztof <krzysztof.turek007@gmail.com>
2024-12-16 16:40:01 +0100

    07/2024

---
+++ advent-of-code-2024/07.py
@@ -0,0 +1,21 @@
+# Each line represents a single equation. The test value appears before the colon on each line;
+# it is your job to determine whether the remaining numbers can be combined with operators to produce the test value.
+
+# Operators are always evaluated left-to-right, not according to precedence rules.
+# Furthermore, numbers in the equations cannot be rearranged. Glancing into the jungle,
+# you can see elephants holding two different types of operators: add (+) and multiply (*).
+
+#   For example:
+
+#   190: 10 19
+#   3267: 81 40 27
+#   83: 17 5
+#   156: 15 6
+#   7290: 6 8 6 15
+#   161011: 16 10 13
+#   192: 17 8 14
+#   21037: 9 7 18 13
+#   292: 11 6 16 20
+
+#   test answer 1: 3749
+
