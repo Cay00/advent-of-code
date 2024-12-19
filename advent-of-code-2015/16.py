86a06d68cf988676536f32a6b3225ec93b4b8fc7
Krzysztof <krzysztof.turek007@gmail.com>
2024-12-19 22:38:43 +0100

    16/2015

---
+++ advent-of-code-2015/16.py
@@ -0,0 +1,58 @@
+def parse_input(text):
+    aunts = {}
+
+    for line in text:
+        parts = line.split(": ", 1)
+        name = parts[0]
+        attributes = parts[1].split(", ")
+
+        attributes_dict = {}
+        for prop in attributes:
+            key, value = prop.split(": ")
+            attributes_dict[key] = int(value)
+
+        aunts[name] = attributes_dict
+
+    return aunts
+
+
+def analyse(text):
+    data = parse_input(text)
+
+    for sue, properties in data.items():
+        matches = 0
+
+        for prop, value in properties.items():
+            if prop in mfcsam_results and value != mfcsam_results[prop]:
+                break
+            elif prop in mfcsam_results and value == mfcsam_results[prop]:
+                matches += 1
+
+        if matches == 3:
+            return sue
+    return None
+
+
+mfcsam_results = {
+    "children": 3,
+    "cats": 7,
+    "samoyeds": 2,
+    "pomeranians": 3,
+    "akitas": 0,
+    "vizslas": 0,
+    "goldfish": 5,
+    "trees": 3,
+    "cars": 2,
+    "perfumes": 1,
+}
+
+text = []
+
+while True:
+    line = input()
+    if not line:
+        break
+    text.append(line)
+
+answer = analyse(text)
+print(answer)
