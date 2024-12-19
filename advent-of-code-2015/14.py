38bc110e133ba52e02c505cd84d0e66ee21a46ba
Krzysztof <krzysztof.turek007@gmail.com>
2024-12-19 13:24:26 +0100

    14/2015

--- advent-of-code-2015/14.py
+++ advent-of-code-2015/14.py
@@ -8,32 +8,34 @@ def parse_input(text):
         time = int(words[6])
         rest = int(words[13])
         distance = 0
+        points = 0
 
-        data[person] = (speed, time, rest, distance)
+        data[person] = (speed, time, rest, distance, points)
 
     return data
 
 
 def calculate(text):
     data = parse_input(text)
-    max = 0
+    max_distance = 0
     total_time = 2503  # seconds
+    current_leader = None
 
-    for name, (speed, time, rest, distance) in data.items():
+    for name, (speed, time, rest, distance, points) in data.items():
         cycle_time = time + rest
-        # cycle = 0
+        distance = 0
 
+        # Total distance
         for i in range(total_time):
-            if (i) % cycle_time < time:
+            if i % cycle_time < time:
                 distance += speed
-            # if (i) % cycle_time == 0:
-            #     cycle += 1
 
-        if distance > max:
-            max = distance
-        #print(name, distance)
+        # Max distance
+        if distance > max_distance:
+            max_distance = distance
+            current_leader = name
 
-    return max
+    return current_leader, max_distance
 
 
 text = []
